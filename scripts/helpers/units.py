a4fe852c16ccc2cc9a5932f4374d8c545ddba7f4
reya <reya@otsegosoilandwater.com>
2026-01-05 14:21:09 -0500

    rm whitespace

--- scripts/helpers/units.py
+++ scripts/helpers/units.py
@@ -6,7 +6,6 @@
 #              Full license in LICENSE file, or at <https://www.gnu.org/licenses/>
 # -----------------------------------------------------------------------------------------
 
-
 import arcpy
 from typing import Literal
 
