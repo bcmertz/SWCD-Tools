bdac8265fef4f4adab83c5bd0f8d496c9ffb97b4
reya <reya@otsegosoilandwater.com>
2026-01-08 13:27:09 -0500

    update USC protocol

--- scripts/BufferTools/PointPlots.py
+++ scripts/BufferTools/PointPlots.py
@@ -3,9 +3,9 @@
 # Purpose:     This tool creates point plots for a given analysis area based off
 #              the Upper Susquehanna Coalition (USC) riparian forest buffer
 #              yearly monitoring program rates:
-#                  < 1 acres - plot radius 11.8ft; 10 / acre
-#                  < 5 acres - plot radius 26.3ft; 2 / acre
-#                  > 5 acres - plot radius 26.3ft; 1 / acre
+#                  < 1/2 acres - monitor 100%
+#                  < 3 acres - plot radius 26.3ft; 2 / acre
+#                  > 3 acres - plot radius 26.3ft; 1 / acre
 #              Consider eventually making these user set to allow for more broad
 #              use cases.
 #
@@ -50,27 +50,20 @@ class PointPlots:
         param1.parameterDependencies = [param0.name]
 
         param2 = arcpy.Parameter(
-            displayName="Reduce surveyed area for large (> 5 acres) buffers to 5%?",
-            name="reduce_acreage",
-            datatype="GPBoolean",
-            parameterType="Optional",
-            direction="Input")
-
-        param3 = arcpy.Parameter(
             displayName="Output point plot GPS coordinates to spreadsheet?",
             name="locations",
             datatype="GPBoolean",
             parameterType="Optional",
             direction="Input")
 
-        param4 = arcpy.Parameter(
+        param3 = arcpy.Parameter(
             displayName="Output Folder - Coordinates Spreadsheet",
             name="coordinates_output",
             parameterType="Optional",
             datatype="DEType",
             direction="Input")
 
-        params = [param0, param1, param2, param3, param4]
+        params = [param0, param1, param2, param3]
         return params
 
     def isLicensed(self):
@@ -80,18 +73,18 @@ class PointPlots:
     def updateMessages(self, parameters):
         """Modify the messages created by internal validation for each tool parameter."""
         # make optional parameters[4] required based off of parameters[3]
-        toggle_required_parameter(parameters[3], parameters[4])
+        toggle_required_parameter(parameters[2], parameters[3])
 
         validate(parameters)
         return
 
     def updateParameters(self, parameters):
         # toggle asking for coordinate output path
-        if not parameters[3].hasBeenValidated:
-            if parameters[3].value == True:
-                parameters[4].enabled = True
+        if not parameters[2].hasBeenValidated:
+            if parameters[2].value == True:
+                parameters[3].enabled = True
             else:
-                parameters[4].enabled = False
+                parameters[3].enabled = False
         return
 
     def execute(self, parameters, messages):
@@ -103,9 +96,8 @@ class PointPlots:
         log("reading in parameters")
         planting_area = parameters[0].value
         output_points = parameters[1].valueAsText
-        reduce_acreage = parameters[2].value
-        coords = parameters[3].value
-        output_coords = parameters[4].valueAsText
+        coords = parameters[2].value
+        output_coords = parameters[3].valueAsText
 
         # create scratch layers
         log("creating scratch layers")
@@ -123,61 +115,56 @@ class PointPlots:
         arcpy.management.CalculateGeometryAttributes(in_features=scratch_dissolve, geometry_property=[[field_name, "AREA_GEODESIC"]], area_unit="ACRES_US")
 
         # determine number of sampling plots
-        # < 1 acres - plot radius 11.8ft; 10 / acre
-        # < 5 acres - plot radius 26.3ft; 2 / acre
-        # > 5 acres - plot radius 26.3ft; 1 / acre
+        #   < 1/2 acres - monitor 100%
+        #   < 3 acres - plot radius 26.3ft; 2 / acre
+        #   > 3 acres - plot radius 26.3ft; 1 / acre
         values = [row[0] for row in arcpy.da.SearchCursor(scratch_dissolve, field_name)]
         acreage = max(set(values))
 
-        radius = 11.8
+        radius = 26.3
         num = 1
 
-        if acreage < 1:
-            num = int(math.ceil(acreage * 10))
-        elif acreage >= 1 and acreage < 5:
-            num = int(math.ceil(acreage * 2))
-            radius = 26.3
-        elif acreage >= 5 and not reduce_acreage:
-            num = int(math.ceil(acreage * 2))
-            radius = 26.3
-        elif acreage >= 5 and reduce_acreage:
-            num = int(math.ceil(acreage * 1))
-            radius = 26.3
+        if acreage < 0.5:
+            log("Buffer less than 0.5 acres, please assess 100% of the buffer without point plots")
+        else:
+            elif acreage >= 0.5 and acreage < 3:
+                num = int(math.ceil(acreage * 2))
+            elif acreage >= 3:
+                num = int(math.ceil(acreage * 1))
 
-        # create buffer inside the planting area
-        log("buffer output area")
-        arcpy.analysis.PairwiseBuffer(scratch_dissolve, scratch_buffer, "{} Feet".format(-radius))
+            # create buffer inside the planting area
+            log("buffer output area")
+            arcpy.analysis.PairwiseBuffer(scratch_dissolve, scratch_buffer, "{} Feet".format(-radius))
 
-        # create random plot centers
-        log("create sampling locations")
-        arcpy.management.CreateSpatialSamplingLocations(scratch_buffer, output_points, sampling_method="STRAT_POLY", strata_id_field=None, strata_count_method="PROP_AREA", num_samples=num, geometry_type="POINT", min_distance="{} Feet".format(radius*2))
+            # create random plot centers
+            log("create sampling locations")
+            arcpy.management.CreateSpatialSamplingLocations(scratch_buffer, output_points, sampling_method="STRAT_POLY", strata_id_field=None, strata_count_method="PROP_AREA", num_samples=num, geometry_type="POINT", min_distance="{} Feet".format(radius*2))
 
-        # add data to map
-        log("add data to map")
-        points_lyr = active_map.addDataFromPath(output_points)
+            # add data to map
+            log("add data to map")
+            points_lyr = active_map.addDataFromPath(output_points)
 
-        if coords:
-            log("calculating point x y coordinates")
-            # add coordinate fields
-            arcpy.management.AddField(points_lyr, "x_coord", "FLOAT", 6, 6, field_alias="X Coordinate")
-            arcpy.management.AddField(points_lyr, "y_coord", "FLOAT", 6, 6, field_alias="X Coordinate")
+            if coords:
+                log("calculating point x y coordinates")
+                # add coordinate fields
+                arcpy.management.AddField(points_lyr, "x_coord", "FLOAT", 6, 6, field_alias="X Coordinate")
+                arcpy.management.AddField(points_lyr, "y_coord", "FLOAT", 6, 6, field_alias="X Coordinate")
 
-            # calculate geometry - coordinates
-            arcpy.management.CalculateGeometryAttributes(in_features=points_lyr.name, geometry_property=[["x_coord", "POINT_X"]],coordinate_format="DD")
-            arcpy.management.CalculateGeometryAttributes(in_features=points_lyr.name, geometry_property=[["y_coord", "POINT_Y"]],coordinate_format="DD")
+                # calculate geometry - coordinates
+                arcpy.management.CalculateGeometryAttributes(in_features=points_lyr.name, geometry_property=[["x_coord", "POINT_X"]],coordinate_format="DD")
+                arcpy.management.CalculateGeometryAttributes(in_features=points_lyr.name, geometry_property=[["y_coord", "POINT_Y"]],coordinate_format="DD")
 
-            # export attribute table to csv at path
-            log("exporting point plot coordinates")
-            arcpy.conversion.ExportTable(output_points, r"{}/point_plots.csv".format(output_coords))
+                # export attribute table to csv at path
+                log("exporting point plot coordinates")
+                arcpy.conversion.ExportTable(output_points, r"{}/point_plots.csv".format(output_coords))
+
+                # open coordinates folder
+                log("opening folder with coordinates")
+                os.startfile(output_coords)
 
         # cleanup
         log("deleting unneeded data")
         empty_workspace(arcpy.env.scratchGDB, keep=[])
-
-        # open coordinates folder
-        if coords:
-            log("opening folder with coordinates")
-            os.startfile(output_coords)
 
         # save
         log("saving project")
