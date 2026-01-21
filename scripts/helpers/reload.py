06ee8581931c10bcbb6ce59cfc708aac364519a1
reya <leah.mertz@protonmail.com>
2026-01-21 09:07:53 -0500

    force boolean and documentation

--- scripts/helpers/reload.py
+++ scripts/helpers/reload.py
@@ -1,8 +1,9 @@
 # -----------------------------------------------------------------------------------------
 # Name:        Reload
-# Purpose:     This package provides the reload_module decorator for the execute method of
-#              a tool. This decorator reloads a module when it is ran to pick up any code
-#              changes since it was last ran.
+# Purpose:     This package provides the reload_module decorator. When applied to the
+#              the execute method of a tool, this decorator fully reloads the module
+#              prior to running the tool. This enables us to pick up any code changes
+#              since it was last run for ease of development.
 #
 # License:     GNU Affero General Public License v3.
 #              Full license in LICENSE file, or at <https://www.gnu.org/licenses/>
@@ -13,14 +14,18 @@ import os
 from importlib import import_module
 from functools import wraps
 
-def reload_module(name):
+def reload_module(name, force=False):
+    """reload_module provides a function decorator which forces the module of NAME whose execute method it
+    decorates to be reloaded. This only occurs when the swcdtools_dev environmental variable is set or the
+    FORCE boolean is passed to it."""
+
     def reload_module(func):
         # this provides __wrapped__ on the underlying decorated function
         # we use this later to call our origin function without the decorator
         # to avoid infinite recursion
         @wraps(func)
         def wrapper(self, parameters, messages):
-            if os.environ.get('swcdtools_dev'):
+            if os.environ.get('swcdtools_dev') or force:
                 class_name = self.__class__.__name__
                 # delete the module we're executing and re-import it
                 for module in list(sys.modules):
