850de5f855adf4dd21ccf3151a222fbc25af727e
reya <bennett.mertz@gmail.com>
2026-01-24 15:14:31 -0500

    Update reload.py

--- scripts/helpers/reload.py
+++ scripts/helpers/reload.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------------------
-# Name:        Reload
+# Name:        Reload Module
 # Purpose:     This package provides the reload_module decorator. When applied to the
 #              the execute method of a tool, this decorator fully reloads the module
 #              prior to running the tool. This enables us to pick up any code changes
@@ -14,29 +14,31 @@ import os
 from importlib import import_module
 from functools import wraps
 
-def reload_module(name, force=False):
-    """reload_module provides a function decorator which forces the module of NAME whose execute method it
-    decorates to be reloaded. This only occurs when the swcdtools_dev environmental variable is set or the
-    FORCE boolean is passed to it."""
-
+def reload_module(name, force=True):
+    """reload_module reads in the NAME and FORCE arguments and returns the 
+    actual reload_module function decorator."""
+    
     def reload_module(func):
-        """this provides __wrapped__ on the underlying decorated function
-        we use this later to call our origin function without the decorator
-        to avoid infinite recursion"""
+        """reload_module takes the original execute FUNC and returns a wrapper
+        function with additional logic to run prior to either executing FUNC or
+        reloading it."""
 
-        @wraps(func)
+        @wraps(func) # provide __wrapped__ method on execute to avoid calling decorator again
         def wrapper(self, parameters, messages):
-            if os.environ.get('swcdtools_dev') or force:
-                class_name = self.__class__.__name__
+            """wrapper checks whether to load any code changes based off FORCE,
+            and either calls the new execute method or the original."""
+
+            if force:
                 # delete the module we're executing and re-import it
+                class_name = self.__class__.__name__
                 for module in list(sys.modules):
                     if name in module:
                         del sys.modules[module]
                 out = import_module(name)
-                # we can't just call return func(self, parameters, messages) since this wont pick
-                # up the refreshed modules so we have to get the code from the output module
+                # call the updated execute method without it's decorator
                 return out.__dict__[class_name].execute.__wrapped__(self, parameters, messages)
             else:
+                # call the original execute method
                 return func(self, parameters, messages)
         return wrapper
     return reload_module
