2ffcf302a8940f30d1a4c500026ea196df6ce8db
reya <leah.mertz@protonmail.com>
2026-02-10 16:34:38 -0500

    descriptions

--- scripts/TileDrainage/__init__.py
+++ scripts/TileDrainage/__init__.py
@@ -1,3 +1,11 @@
+# --------------------------------------------------------------------------------
+# Name:        Tile Drainage Package
+# Purpose:     Collect tile drainage tools into a package
+#
+# License:     GNU Affero General Public License v3.
+#              Full license in LICENSE file, or at <https://www.gnu.org/licenses/>
+# --------------------------------------------------------------------------------
+
 from .DecisionTree import DecisionTree
 from .ImageDifferencingSetup import ImageDifferencingSetup
 from .ImageDifferencing import ImageDifferencing
