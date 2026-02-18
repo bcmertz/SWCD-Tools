383915d3a366f06b6dd457b1c42e0f2296854227
reya <leah.mertz@protonmail.com>
2026-02-18 08:20:22 -0500

    streambank likelihood import

--- scripts/FluvialGeomorphology/__init__.py
+++ scripts/FluvialGeomorphology/__init__.py
@@ -10,4 +10,5 @@ from .StreamCenterlineAdjuster import LeastAction
 from .StreamElevation import StreamElevation
 from .StreamNetwork import StreamNetwork
 from .StreambankDetection import StreambankDetection
+from .StreambankDetectionLikelihood import StreambankDetectionLikelihood
 from .GenerateCrossSections import GenerateCrossSections, generate_transects, transect_line
