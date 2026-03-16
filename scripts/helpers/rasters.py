c06d560491a96c637950926e00e4ab45d8f9de8c
reya <leah.mertz@protonmail.com>
2026-03-16 16:29:58 -0400

    cell_area and cells_per_area

--- scripts/helpers/rasters.py
+++ scripts/helpers/rasters.py
@@ -7,7 +7,8 @@
 # -----------------------------------------------------------------------------------
 
 import arcpy
-import math
+
+from .units import convert_area, LINEAR_TO_AREAL
 
 PIXEL_TYPES = {
     "U1": "1_BIT",
@@ -27,23 +28,34 @@ def pixel_type(raster):
     """Return the the string representation of the raster pixel type."""
     return PIXEL_TYPES[raster.pixelType]
 
-
-def cell_size(raster, output_unit_source=None):
-    """Return the cell size of a raster in the linear units of another data source."""
-    output_unit = "FeetUS"
-    if output_unit_source:
-        output_unit = arcpy.Describe(output_unit_source).spatialReference.linearUnitName
-
+def cell_area(raster) -> str:
+    """Return the cell size of a raster as a GPArealUnit."""
     # Note: throws an error if not a raster, this is desirable and shouldn't be used on
     # data types other than a raster
     desc_raster = arcpy.Describe(raster)
-    raster_linear_unit = desc_raster.spatialReference.linearUnitName
+    linear_unit = desc_raster.spatialReference.linearUnitName
+    square_unit = LINEAR_TO_AREAL[linear_unit]
 
     # Cell size in the X and Y axis
-    cellsize_y = desc_raster.meanCellHeight * arcpy.LinearUnitConversionFactor(raster_linear_unit, output_unit)
-    cellsize_x = desc_raster.meanCellWidth * arcpy.LinearUnitConversionFactor(raster_linear_unit, output_unit)
+    cellsize_y = desc_raster.meanCellHeight
+    cellsize_x = desc_raster.meanCellWidth
+    area=cellsize_x * cellsize_y
 
-    return math.sqrt(cellsize_x * cellsize_y)
+    # create unit
+    unit = "{} {}".format(area, square_unit)
+
+    return unit
+
+def cells_per_area(raster, area: str) -> int:
+    """Convert GPArealUnit AREA to the number of cells in the RASTER it is equivalent to."""
+    cell_size, cell_unit = cell_area(raster).split(" ")
+
+    # convert area to raster cell unit
+    area_size_in_cell_units = convert_area(area, cell_unit).split(" ")[0]
+
+    # find number of cells
+    num_cells = area_size_in_cell_units / cell_size
+    return num_cells
 
 def min_cell_path(parameters):
     """Return the parameter with the smallest cell size."""
@@ -51,9 +63,13 @@ def min_cell_path(parameters):
     min_cell_path = None
     for param in parameters:
         try:
-            size = cell_size(param.value)
-            if min_cell_size is None or size < min_cell_size:
-                min_cell_size = size
+            # get GPArealUnit of param and convert to US Acres
+            raster_cell_area = cell_area(param.value)
+            size_acres, unit = convert_area(raster_cell_area, "AcresUS").split(" ")
+
+            # compare sizes
+            if min_cell_size is None or size_acres < min_cell_size:
+                min_cell_size = size_acres
                 min_cell_path = param.valueAsText
         except:
             pass
