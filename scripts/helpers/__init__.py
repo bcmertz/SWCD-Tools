59188d3f27e18430e5ebfcb4c8ece2a57ebc1731
reya <leah.mertz@protonmail.com>
2026-03-16 16:29:34 -0400

    cell_area and cells_per_area

--- scripts/helpers/__init__.py
+++ scripts/helpers/__init__.py
@@ -9,6 +9,6 @@
 from .layers import get_oid, add_layer_to_group
 from .logging import log, warn, error
 from .parameter import validate_spatial_reference, toggle_required_parameter, sanitize
-from .rasters import pixel_type, cell_size, min_cell_path
+from .rasters import pixel_type, cell_area, min_cell_path, cells_per_area
 from .tool import license, setup_environment, reload_module, empty_workspace
 from .units import get_z_unit, get_linear_unit, LINEAR_UNITS, AREAL_UNITS, LINEAR_TO_AREAL, convert_area
