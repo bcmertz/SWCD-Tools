93346e4d3da2a8b7beccf77af0f40f46bb99d8cf
reya <leah.mertz@protonmail.com>
2026-03-17 11:06:26 -0400

    watershed size raster tool

--- scripts/Hydrology/WatershedSize.py
+++ scripts/Hydrology/WatershedSize.py
@@ -7,14 +7,14 @@
 # --------------------------------------------------------------------------------
 import arcpy
 
-from ..helpers import license, empty_workspace, reload_module, log, Z_UNITS, get_z_unit
+from ..helpers import license, reload_module, log, AREAL_UNITS, AREAL_UNITS_MAP, cell_area, convert_area
 from ..helpers import setup_environment as setup
 from ..helpers import validate_spatial_reference as validate
 
 class WatershedSize:
     def __init__(self):
         """Define the tool (tool name is the name of the class)."""
-        self.label = "Watershed Size"
+        self.label = "Watershed Size Raster"
         self.category = "Hydrology"
         self.description = "Calculate Watershed Size"
 
@@ -28,41 +28,34 @@ class WatershedSize:
             direction="Input")
 
         param1 = arcpy.Parameter(
-            displayName="Z Unit",
-            name="z_unit",
+            displayName="Analysis Area",
+            name="analysis_area",
+            datatype="GPExtent",
+            parameterType="Optional",
+            direction="Input")
+        param1.controlCLSID = '{15F0D1C1-F783-49BC-8D16-619B8E92F668}'
+
+        param2 = arcpy.Parameter(
+            displayName="Watershed Area Unit",
+            name="unit",
             datatype="GPString",
             parameterType="Required",
             direction="Input")
-        param1.filter.list = Z_UNITS
+        param2.filter.list = AREAL_UNITS
+        param2.value = "US Survey Acres"
 
-        param2 = arcpy.Parameter(
-            displayName="Output Folder",
-            name="output_location",
-            datatype="DEFolder",
-            parameterType="Required",
-            direction="Input")
+        param3 = arcpy.Parameter(
+            displayName="Output Raster",
+            name="out_features",
+            datatype="DEFeatureClass",
+            direction="Output")
+        param3.parameterDependencies = [param0.name, param2.name]
+        param3.schema.clone = True
 
-        params = [param0, param1, param2]
+        params = [param0, param1, param2, param3]
         return params
 
     def updateParameters(self, parameters):
-        # find z unit of raster based on vertical coordinate system
-        #  - if there is none, let the user define it
-        #  - if it exists, set the value and hide the parameter
-        #  - if it doesn't exist show the parameter and set the value to None
-        if not parameters[0].hasBeenValidated:
-            if parameters[0].value:
-                z_unit = get_z_unit(parameters[0].value)
-                if z_unit:
-                    parameters[1].enabled = False
-                    parameters[1].value = z_unit
-                else:
-                    parameters[1].enabled = True
-                    parameters[1].value = None
-            else:
-                parameters[1].enabled = False
-                parameters[1].value = None
-
         return
 
     def updateMessages(self, parameters):
@@ -83,16 +76,41 @@ class WatershedSize:
 
         # read in parameters
         log("reading in parameters")
-        raster_layer = parameters[0].value
-        z_unit = parameters[1].value
-        output_folder_path = parameters[2].valueAsText
+        dem = parameters[0].value
+        extent = parameters[1].value
+        # read in areal unit and map it's pretty string to the arcpy representation
+        areal_unit = AREAL_UNITS_MAP[parameters[2].valueAsText]
+        output_file = parameters[3].valueAsText
 
-        log(raster_layer, z_unit, output_folder_path)
+        # set analysis extent
+        if extent:
+            arcpy.env.extent = extent
 
-        # cleanup
-        log("deleting unneeded data")
-        empty_workspace(arcpy.env.scratchGDB, keep=[])
-        arcpy.management.Delete([])
+        # fill raster
+        log("filling raster")
+        fill_dem = arcpy.sa.Fill(dem)
+
+        # flow direction
+        log("calculating flow direction")
+        flow_direction = arcpy.sa.FlowDirection(fill_dem)
+
+        # flow accumulation
+        log("calculating flow accumulation")
+        flow_accumulation = arcpy.sa.FlowAccumulation(flow_direction)
+
+        # convert flow accumulation from number of cells to threshold area units
+        log("calculating watershed size")
+        raster_cell_area = cell_area(dem)
+        cell_size = convert_area(raster_cell_area, areal_unit).split(" ")[0]
+        watershed_size = flow_accumulation * float(cell_size)
+
+        # save output to file
+        log("saving output to file")
+        watershed_size.save(output_file)
+
+        # add watershed size raster to map
+        log("adding watershed size raster to map")
+        active_map.addDataFromPath(output_file)
 
         # save program successfully
         log("saving project")
