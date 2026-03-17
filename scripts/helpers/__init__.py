a4ac9c479bcc2043bb52f5de0fc06a8cb9b5c08e
reya <leah.mertz@protonmail.com>
2026-03-17 11:06:16 -0400

    fix units

--- scripts/helpers/__init__.py
+++ scripts/helpers/__init__.py
@@ -11,4 +11,5 @@ from .logging import log, warn, error
 from .parameter import validate_spatial_reference, toggle_required_parameter, sanitize
 from .rasters import pixel_type, cell_area, min_cell_path, cells_per_area
 from .tool import license, setup_environment, reload_module, empty_workspace
-from .units import get_z_unit, get_linear_unit, Z_UNITS, LINEAR_UNITS, AREAL_UNITS, SPATIAL_TO_LINEAR, LINEAR_TO_AREAL, convert_area
+from .units import get_z_unit, get_linear_unit, Z_UNITS, LINEAR_UNITS, AREAL_UNITS, \
+    LINEAR_UNITS_MAP, AREAL_UNITS_MAP, SPATIAL_TO_LINEAR, LINEAR_TO_AREAL, convert_area
--- scripts/helpers/units.py
+++ scripts/helpers/units.py
@@ -30,34 +30,90 @@ def get_linear_unit(fc) -> str | None:
 # mapping of spatial reference linear unit to GPLinearUnit
 SPATIAL_TO_LINEAR = {
     "Meter": "Meters",
-    "Foot_US": "FeetUS",
+    "Foot_US": "Feet",
     "Foot": "FeetInt"
 }
 
 # z-units available to rasters for VCS
 Z_UNITS = list(SPATIAL_TO_LINEAR.keys())
 
-# mapping of GPLinearUnit to GPArealUnit (square units)
-LINEAR_TO_AREAL = {
-    "Kilometers": "SquareKilometers",
-    "Meters": "SquareMeters",
-    "Decimeters": "SquareDecimeters",
-    "Centimeters": "SquareCentimeters",
-    "Millimeters": "SquareMillimeters",
-    "YardsInt": "SquareYards",
-    "FeetInt": "SquareFeet",
-    "InchesInt": "SquareInches",
-    "MilesInt": "SquareMiles",
-    "MilesUS": "SquareMilesUS",
-    "YardsUS": "SquareYardsUS",
-    "FeetUS": "SquareFeetUS",
-    "InchesUS": "SquareInchesUS ",
-    "NauticalMilesUS": "Unknown",
-    "NauticalMilesInt": "Unknown",
+# inferred from https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gplinearunit
+# but accuracy is unclear since they only give "esriFeet" and other placeholders
+# to test accuracy every GPLinearUnit was logged in a script
+#
+# maps parameter display representation to arcpy GPLinearUnit
+LINEAR_UNITS_MAP = {
+    "Unknown": "Unknown",
+    "International Inches": "InchesInt",
+    "US Survey Inches": "Inches",
+    "International Feet": "FeetInt",
+    "US Survey Feet": "Feet",
+    "International Yards": "YardsInt",
+    "US Survey Yards": "Yards",
+    "Statute Miles": "MilesInt",
+    "US Survey Miles": "Miles",
+    "Millimeters": "Millimeters",
+    "Centimeters": "Centimeters",
+    "Decimeters": "Decimeters",
+    "Meters": "Meters",
+    "Kilometers": "Kilometers",
+    "US Survey Nautical Miles": "NauticalMiles",
+    "International Nautical Miles": "NauticalMilesInt",
+    "Points": "Points",
+    "Decimal Degrees": "DecimalDegrees",
 }
+LINEAR_UNITS = list(LINEAR_UNITS_MAP.keys())
 
-LINEAR_UNITS = list(LINEAR_TO_AREAL.keys())
-AREAL_UNITS = list(LINEAR_TO_AREAL.values())
+# https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gparealunit
+#
+# maps parameter display representation to arcpy GPArealUnit
+AREAL_UNITS_MAP = {
+    "Unknown": "Unknown",
+    "Square International Inches": "SquareInches",
+    "Square US Inches": "SquareInchesUS",
+    "Square International Feet": "SquareFeet",
+    "Square US Feet": "SquareFeetUS",
+    "Square International Yards": "SquareYards",
+    "Square US Yards": "SquareYardsUS",
+    "International Acres": "Acres",
+    "US Survey Acres": "AcresUS",
+    "Square Statute Miles": "SquareMiles",
+    "Square US Survey Miles": "SquareMilesUS",
+    "Square Millimeters": "SquareMillimeters",
+    "Square Centimeters": "SquareCentimeters",
+    "Square Decimeters": "SquareDecimeters",
+    "Square Meters": "SquareMeters",
+    "Square Kilometers": "SquareKilometers",
+    "Ares": "Ares",
+    "Hectares": "Hectares",
+}
+AREAL_UNITS = list(AREAL_UNITS_MAP.keys())
+
+# mapping of GPLinearUnit to GPArealUnit (square units)
+# not all units have a mapping
+#
+# kind of annoying that the naming conventions change between
+# linear and areal with respect to US / Int suffix
+LINEAR_TO_AREAL = {
+    "Unknown": "Unknown",
+    "Inches": "SquareInchesUS",
+    "InchesInt": "SquareInches",
+    "Feet": "SquareFeetUS",
+    "FeetInt": "SquareFeet",
+    "Yards": "SquareYardsUS",
+    "YardsInt": "SquareYards",
+    "Miles": "SquareMilesUS",
+    "MilesInt": "SquareMiles",
+    "Millimeters": "SquareMillimeters",
+    "Centimeters": "SquareCentimeters",
+    "Decimeters": "SquareDecimeters",
+    "Meters": "SquareMeters",
+    "Kilometers": "SquareKilometers",
+    "NauticalMiles": "Unknown",
+    "NauticalMilesInt": "Unknown",
+    "Points": "Unknown",
+    "DecimalDegrees": "Unknown",
+}
 
 def convert_area(area: str, output_unit: str) -> str:
     """Convert AREA to OUTPUT_UNIT factoring in area size."""
