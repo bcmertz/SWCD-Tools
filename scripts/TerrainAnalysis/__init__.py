47cbbf935073a708635950abfd475c53a72c27fa
reya <bennett.mertz@protonmail.com>
2026-06-08 14:03:15 -0400

    separate rem into function

--- scripts/TerrainAnalysis/__init__.py
+++ scripts/TerrainAnalysis/__init__.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------------
 
 from .PotentialWetlands import PotentialWetlands
-from .REMCalculator import RelativeElevationModel
+from .REMCalculator import RelativeElevationModel, relative_elevation_model
 from .StreamPowerIndex import StreamPowerIndex
 from .TopographicWetness import TopographicWetness
 from .TopographicPositionIndex import TopographicPositionIndex, topographic_position_index
