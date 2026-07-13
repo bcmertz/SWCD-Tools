2a879d347f0259766b74f5426aeee1b2c9fa4a50
reya <bennett.mertz@protonmail.com>
2026-07-13 11:14:08 -0400

    ensure statistics will be correct

--- src/TerrainAnalysis/VBET.py
+++ src/TerrainAnalysis/VBET.py
@@ -198,6 +198,7 @@ class VBET(object):
         log("creating scratch layers")
         scratch_buffer = arcpy.CreateScratchName("buffer", data_type="FeatureClass", workspace=arcpy.env.scratchGDB)
         scratch_area = arcpy.CreateScratchName("area", data_type="FeatureClass", workspace=arcpy.env.scratchGDB)
+        max_scratch = arcpy.CreateScratchName("max", data_type="RasterDataset", workspace=arcpy.env.scratchGDB)
 
         # buffer streams, each stream segment a separate buffer segment
         log("creating buffer around stream lines")
@@ -294,14 +295,14 @@ class VBET(object):
             cellsize_type = "MinOf",
 	    ignore_nodata = True,
         )
-        max_stats = arcpy.management.CalculateStatistics(max_raster)
+        max_raster.save(max_scratch)
 
         # threshold to full valley bottom and low-lying valley bottom
         #
         # full valley bottom = 0.65, low lying valley bottom = 0.85
         log("thresholding output probability to find full valley bottom and low-lying valley bottom areas")
-        full_valley = arcpy.sa.Con(max_stats, 1, where_clause="VALUE >= 0.65")
-        low_lying = arcpy.sa.Con(max_stats, 1, where_clause="VALUE >= 0.85")
+        full_valley = arcpy.sa.Con(max_scratch, 1, where_clause="VALUE >= 0.65")
+        low_lying = arcpy.sa.Con(max_scratch, 1, where_clause="VALUE >= 0.85")
 
         # polygonize outputs
         log("converting outputs to polygons")
