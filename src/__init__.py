141c76e88e71072d50e489a798985a3af3cd0e2a
reya <leah.mertz@protonmail.com>
2026-07-16 20:48:54 -0400

    reconfigure package structure

--- src/__init__.py
+++ src/__init__.py
@@ -5,12 +5,3 @@
 # License:     Contextual Copyleft AI (CCAI) License v1.0.
 #              Full license in LICENSE file.
 # --------------------------------------------------------------------------------
-
-from .AgAssessment import *
-from .BufferTools import *
-from .FluvialGeomorphology import *
-from .Hydrology import *
-from .Utilities import *
-from .TerrainAnalysis import *
-from .TerrainModification import *
-from .TileDrainage import *
