df1379b12e6f696af76a0b83a44a8a4b48aebccc
reya <bennett.mertz@protonmail.com>
2026-08-04 18:10:14 -0400

    update unit handling

--- src/helpers/units.py
+++ src/helpers/units.py
@@ -10,60 +10,7 @@ import arcpy
 from copy import copy
 from typing import Self
 from functools import singledispatchmethod
-from enum import StrEnum
-
-# inferred from https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gplinearunit
-# but accuracy is unclear since they only give "esriFeet" and other placeholders
-# to test accuracy every GPLinearUnit was logged in a script
-#
-# map arcpy GPLinearUnit to parameter display representation
-LINEAR_UNITS_MAP = {
-    "Unknown": "Unknown",
-    "International Inches": "InchesInt",
-    "US Survey Inches": "Inches",
-    "International Feet": "FeetInt",
-    "US Survey Feet": "Feet",
-    "International Yards": "YardsInt",
-    "US Survey Yards": "Yards",
-    "Statute Miles": "MilesInt",
-    "US Survey Miles": "Miles",
-    "Millimeters": "Millimeters",
-    "Centimeters": "Centimeters",
-    "Decimeters": "Decimeters",
-    "Meters": "Meters",
-    "Kilometers": "Kilometers",
-    "US Survey Nautical Miles": "NauticalMiles",
-    "International Nautical Miles": "NauticalMilesInt",
-    "Points": "Points",
-    "Decimal Degrees": "DecimalDegrees",
-}
-LINEAR_UNITS = StrEnum("LINEAR_UNITS", {i: i for i in LINEAR_UNITS_MAP.values()})
-
-
-# https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gparealunit
-#
-# map arcpy GPArealUnit to parameter display representation
-AREAL_UNITS_MAP = {
-    "Unknown": "Unknown",
-    "Square International Inches": "SquareInches",
-    "Square US Inches": "SquareInchesUS",
-    "Square International Feet": "SquareFeet",
-    "Square US Feet": "SquareFeetUS",
-    "Square International Yards": "SquareYards",
-    "Square US Yards": "SquareYardsUS",
-    "International Acres": "Acres",
-    "US Survey Acres": "AcresUS",
-    "Square Statute Miles": "SquareMiles",
-    "Square US Survey Miles": "SquareMilesUS",
-    "Square Millimeters": "SquareMillimeters",
-    "Square Centimeters": "SquareCentimeters",
-    "Square Decimeters": "SquareDecimeters",
-    "Square Meters": "SquareMeters",
-    "Square Kilometers": "SquareKilometers",
-    "Ares": "Ares",
-    "Hectares": "Hectares",
-}
-AREAL_UNITS = StrEnum("AREAL_UNITS", {i: i for i in AREAL_UNITS_MAP.values()})
+from enum import StrEnum, auto
 
 
 # mapping of GPLinearUnit to GPArealUnit (square units)
@@ -96,19 +43,111 @@ LINEAR_TO_AREAL = {
 SPATIAL_TO_LINEAR = {
     "Meter": "Meters",
     "Foot_US": "Feet",
-    "Foot": "FeetInt"
+    "Foot": "FeetInt",
 }
 
-# z-units available to rasters for VCS
-Z_UNITS = list(SPATIAL_TO_LINEAR.keys())
+class UNITS(StrEnum):
+    """TODO."""
+    # Handle the name or value of a unit being passed at creation time
+    # Note: only applies to function invocation (eg - LINEAR_UNITS() not LINEAR_UNITS[])
+    @classmethod
+    def _missing_(cls, value):
+        value = value.lower()
+        for member in cls:
+            if member.value == value or member.name == value:
+                return member
+        return None
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.name
+    def display(self):
+        return self.value
+    def __iter__(self):
+        return [i.name for i in LINEAR_UNITS]
+
+# inferred from https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gplinearunit
+# but accuracy is unclear since they only give "esriFeet" and other placeholders
+# to test accuracy every GPLinearUnit was logged in a script
+#
+# map arcpy GPLinearUnit to parameter display representation
+class LINEAR_UNITS(UNITS):
+    Unknown = "Unknown"
+    InchesInt = "International Inches"
+    Inches = "US Survey Inches"
+    FeetInt = "International Feet"
+    Feet = "US Survey Feet"
+    YardsInt = "International Yards"
+    Yards = "US Survey Yards"
+    MilesInt = "Statute Miles"
+    Miles = "US Survey Miles"
+    Millimeters = "Millimeters"
+    Centimeters = "Centimeters"
+    Decimeters = "Decimeters"
+    Meters = "Meters"
+    Kilometers = "Kilometers"
+    NauticalMiles = "US Survey Nautical Miles"
+    NauticalMilesInt = "International Nautical Miles"
+    Points = "Points"
+    DecimalDegrees = "Decimal Degrees"
+
+    def to_areal(self):
+        return AREAL_UNITS[LINEAR_TO_AREAL[self.name]]
+
+    def to_spatial(self):
+        return SPATIAL_UNITS[SPATIAL_TO_LINEAR[self.name]]
 
 
-def get_z_unit(fc) -> LINEAR_UNITS | None:
+class SPATIAL_UNITS(UNITS):
+    Meter = auto()
+    Foot_US = auto()
+    Foot = auto()
+
+    def to_areal(self):
+        return AREAL_UNITS[LINEAR_TO_AREAL[SPATIAL_TO_LINEAR[self.name]]]
+
+    def to_linear(self):
+        return LINEAR_UNITS[SPATIAL_TO_LINEAR[self.name]]
+
+
+# https://developers.arcgis.com/rest/services-reference/enterprise/gp-data-types/#gparealunit
+#
+# map arcpy GPArealUnit to parameter display representation
+class AREAL_UNITS(UNITS):
+    Unknown = "Unknown"
+    SquareInches = "Square International Inches"
+    SquareInchesUS = "Square US Inches"
+    SquareFeet = "Square International Feet"
+    SquareFeetUS = "Square US Feet"
+    SquareYards = "Square International Yards"
+    SquareYardsUS = "Square US Yards"
+    Acres = "International Acres"
+    AcresUS = "US Survey Acres"
+    SquareMiles = "Square Statute Miles"
+    SquareMilesUS = "Square US Survey Miles"
+    SquareMillimeters = "Square Millimeters"
+    SquareCentimeters = "Square Centimeters"
+    SquareDecimeters = "Square Decimeters"
+    SquareMeters = "Square Meters"
+    SquareKilometers = "Square Kilometers"
+    Ares = "Ares"
+    Hectares = "Hectares"
+
+    def to_linear(self):
+        unit = self.name
+        for key, value in LINEAR_TO_AREAL.items():
+            if value == unit:
+                unit = key
+                break
+        return LINEAR_UNITS[unit]
+
+
+def get_z_unit(fc) -> SPATIAL_UNITS | None:
     """Get z unit from spatial reference."""
     # find z unit of spatial reference vertical coordinate system
     desc = arcpy.Describe(fc)
     if desc.spatialReference.VCS:
-        return LINEAR_UNITS[SPATIAL_TO_LINEAR[desc.spatialReference.VCS.linearUnitName]]
+        SPATIAL_UNITS[desc.spatialReference.VCS.linearUnitName]
 
     return None
 
@@ -122,7 +161,7 @@ def get_linear_unit(fc) -> LINEAR_UNITS:
     except Exception:
         return LINEAR_UNITS[fc.spatialReference.linearUnitName]
 
-class BaseUnit:
+class BaseAmount:
     def __init__(self: Self, amount: float | int, unit: LINEAR_UNITS | AREAL_UNITS):
         self.amount = amount
         self.base_unit = unit
@@ -151,35 +190,30 @@ class BaseUnit:
         out.amount = out.amount // divisor
         return out
 
-class LinearUnit(BaseUnit):
+class Length(BaseAmount):
     # this project's type checker ty doesn't support singledispatchmethod yet :/
     # (https://github.com/astral-sh/ty/issues/2805)
     @singledispatchmethod
     def __init__(self, input):
         """Accepts distance and unit either:
         1) Separately:
-           - LinearUnit(4, "Feet")
-           - LinearUnit(5, "International Feet")
-           - LinearUnit(4, LINEAR_UNITS.FeetInt)
+           - Length(4, "Feet")
+           - Length(5, "International Feet")
+           - Length(4, LINEAR_UNITS.FeetInt)
         2) Together:
-           - LinearUnit("4 Feet")
-           - LinearUnit("5 International Feet")
+           - Length("4 Feet")
+           - Length("5 International Feet")
         """
         raise TypeError("Parameters must either be one of 1) input: str, unit: None 2) input: float | int, unit: LINEAR_UNITS. Received {}".format(input))
+    @__init__.register
+    def _(self, length: int | float, unit: str | LINEAR_UNITS):
+        super().__init__(amount=length, unit=LINEAR_UNITS[unit])
     @__init__.register
     def _(self, quantity: str):
         length, unit, *rest = quantity.split(" ")
         if rest:
             unit += " " + " ".join(rest)
-            unit = LINEAR_UNITS_MAP[unit]
         super().__init__(amount=float(length), unit=LINEAR_UNITS[unit])
-    @__init__.register
-    def _(self, length: int | float, unit: str | LINEAR_UNITS):
-        unit, *rest = unit.split(" ")
-        if rest:
-            unit += " " + " ".join(rest)
-            unit = LINEAR_UNITS_MAP[unit]
-        super().__init__(amount=length, unit=LINEAR_UNITS[unit])
     @property
     def length(self) -> int | float:
         return self.amount
@@ -195,84 +229,71 @@ class LinearUnit(BaseUnit):
         self.base_unit = unit
         return
     def to_unit(self: Self, output_unit: LINEAR_UNITS) -> Self:
-        """Convert LinearUnit to output_unit factoring in length size."""
-        self.length = self.length * arcpy.LinearUnitConversionFactor(self.unit, output_unit)
+        """Convert Length to output_unit factoring in length size."""
+        self.length = self.length * arcpy.LengthConversionFactor(self.unit, output_unit)
         self.unit = output_unit
         return self
-    def full_unit(self: Self) -> str:
-        """Return full string description of LINEAR_UNIT stored in LINEAR_UNITS_MAP."""
-        unit = self.unit
-        for key, value in LINEAR_UNITS_MAP.items():
-            if value == unit:
-                unit = key
-                break
-        return unit
     def __eq__(self: Self, other) -> bool:
         # Equals
-        if not isinstance(other, LinearUnit):
+        if not isinstance(other, Length):
             return False
         else:
-            other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+            other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
             return self.length == other_length
     def __ne__(self: Self, other) -> bool:
         # Not equals
         return not self.__eq__(other)
     def __lt__(self: Self, other: Self) -> bool:
         # Less than
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         return self.length < other_length
     def __gt__(self: Self, other: Self) -> bool:
         # Greater than
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         return self.length > other_length
     def __le__(self: Self, other: Self) -> bool:
         # Less or equal
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         return self.length <= other_length
     def __ge__(self: Self, other: Self) -> bool:
         # Greater or equal
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         return self.length >= other_length
     def __add__(self: Self, other: Self) -> Self:
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         out = copy(self)
         out.length += other_length
         return out
     def __sub__(self: Self, other: Self) -> Self:
-        other_length = other.length * arcpy.LinearUnitConversionFactor(other.unit, self.unit)
+        other_length = other.length * arcpy.LengthConversionFactor(other.unit, self.unit)
         out = copy(self)
         out.length -= other_length
         return out
 
-class ArealUnit(BaseUnit):
+class Area(BaseAmount):
     # this project's type checker ty doesn't support singledispatchmethod yet :/
     # (https://github.com/astral-sh/ty/issues/2805)
     @singledispatchmethod
     def __init__(self, input):
         """Accepts area and unit either:
         1) Separately:
-           - ArealUnit(4, "Acres")
-           - ArealUnit(5, "International Acres")
-           - ArealUnit(4, LINEAR_UNITS.Acres)
+           - Area(4, "Acres")
+           - Area(5, "International Acres")
+           - Area(4, LINEAR_UNITS.Acres)
         2) Together:
-           - ArealUnit("4 Acres")
-           - ArealUnit("5 International Acres")
+           - Area("4 Acres")
+           - Area("5 International Acres")
         """
         raise TypeError("Parameters must either be one of 1) input: str, unit: None 2) input: float | int, unit: AREAL_UNITS. Received {}".format(input))
+    @__init__.register
+    def _(self, area: int | float, unit: str | AREAL_UNITS):
+        super().__init__(amount=area, unit=AREAL_UNITS[unit])
     @__init__.register
     def _(self, quantity: str):
         area, unit, *rest = quantity.split(" ")
         if rest:
             unit += " " + " ".join(rest)
-            unit = AREAL_UNITS_MAP[unit]
         super().__init__(amount=float(area), unit=AREAL_UNITS[unit])
-    @__init__.register
-    def _(self, area: int | float, unit: str | AREAL_UNITS):
-        unit, *rest = unit.split(" ")
-        if rest:
-            unit += " " + " ".join(rest)
-            unit = AREAL_UNITS_MAP[unit]
-        super().__init__(amount=area, unit=AREAL_UNITS[unit])
     @property
     def area(self) -> int | float:
         return self.amount
@@ -288,51 +309,43 @@ class ArealUnit(BaseUnit):
         self.base_unit = unit
         return
     def to_unit(self: Self, output_unit: AREAL_UNITS) -> Self:
-        """Convert LinearUnit to output_unit factoring in area size."""
-        self.area = self.area * arcpy.ArealUnitConversionFactor(self.unit, output_unit)
+        """Convert Length to output_unit factoring in area size."""
+        self.area = self.area * arcpy.AreaConversionFactor(self.unit, output_unit)
         self.unit = output_unit
         return self
-    def full_unit(self: Self) -> str:
-        """Return full string description of AREAL_UNIT stored in AREAL_UNITS_MAP."""
-        unit = self.unit
-        for key, value in AREAL_UNITS_MAP.items():
-            if value == unit:
-                unit = key
-                break
-        return unit
     def __eq__(self: Self, other) -> bool:
         # Equals
-        if not isinstance(other, ArealUnit):
+        if not isinstance(other, Area):
             return False
         else:
-            other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+            other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
             return self.area == other_area
     def __ne__(self: Self, other) -> bool:
         # Not equals
         return not self.__eq__(other)
     def __lt__(self: Self, other: Self) -> bool:
         # Less than
-        other_area = other.length * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.length * arcpy.AreaConversionFactor(other.unit, self.unit)
         return self.length < other_area
     def __gt__(self: Self, other: Self) -> bool:
         # Greater than
-        other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
         return self.area > other_area
     def __le__(self: Self, other: Self) -> bool:
         # Less or equal
-        other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
         return self.area <= other_area
     def __ge__(self: Self, other: Self) -> bool:
         # Greater or equal
-        other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
         return self.area >= other_area
     def __add__(self: Self, other: Self) -> Self:
-        other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
         out = copy(self)
         out.area += other_area
         return out
     def __sub__(self: Self, other: Self) -> Self:
-        other_area = other.area * arcpy.ArealUnitConversionFactor(other.unit, self.unit)
+        other_area = other.area * arcpy.AreaConversionFactor(other.unit, self.unit)
         out = copy(self)
         out.area -= other_area
         return out
